3f6340605e285b785f6a8b411b97b3290498cfef
ConfettiBetty <noxiousbyproxy@yahoo.com>
2016-01-05 11:46:23 -0800

    Signed-off-by: ConfettiBetty <noxiousbyproxy@yahoo.com>

---
+++ Class/NoMistake/main.cpp
@@ -0,0 +1,33 @@
+/* 
+  File:   main.cpp
+  Author: Aine (Phyllis) Ukatu
+ Created on January 5, 2016, 11:15 AM
+ Purpose: No Mistakes*/
+//System Libraries
+#include <iostream>
+
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Executive Begins Here
+int main(int argc, char** argv) {
+    //Declare and initialize variables
+    short c,b=20000,a=30000;
+ 
+    //Calculate or map using input or outputs
+    c=a+b;
+    
+    //Output the results
+    cout<<"a = "<<a<<endl;
+    cout<<"b = "<<b<<endl;
+    cout<<c<< "=" <<a<< "+" <<b<<endl;
+    //Exit
+    
+    return 0;
+
+                                }
---
+++ Homework/Savitch_8thEd_Ch1_Prob10/main.cpp
@@ -0,0 +1,36 @@
+/* 
+  File:   main.cpp
+  Author: Aine (Phyllis) Ukatu
+ Created on January 5, 2016, 11:40 AM
+ Purpose: Giant C*/
+//System Libraries
+#include <iostream>
+
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Executive Begins Here
+int main(int argc, char** argv) {
+    //Declare and initialize variables
+    char letter='C';
+     
+    //Output the results
+    cout<<"  "<<letter<<letter<<letter<<endl;
+    cout<<" "<<letter<<"   "<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<" "<<letter<<"   "<<letter<<endl;
+    cout<<"  "<<letter<<letter<<letter<<endl;
+    //Exit
+    
+    return 0;
+
+                                }
