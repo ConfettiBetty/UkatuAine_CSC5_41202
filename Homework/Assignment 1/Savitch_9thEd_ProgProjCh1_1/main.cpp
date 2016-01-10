f8054e6a20d5b988c391ca5acc9a6ac1dc8da70d
Dearie <Dearie>
2016-01-09 17:40:41 -0800

    Signed-off-by: Dearie <Dearie>

---
+++ Homework/Assignment 1/Savitch_9thEd_ProgProjCh1_1/main.cpp
@@ -0,0 +1,34 @@
+/* 
+  File:   main.cpp
+  Author: Aine (Phyllis) Ukatu
+ Created on January 5, 2016, 11:15 AM
+ Purpose: No Mistakes*/
+//System Libraries
+#include <iostream>
+
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Executive Begins Here
+int main(int argc, char** argv) {
+    //Declare and initialize variables
+    short d,c,b=25,a=5;
+    //Calculate or map using input or outputs
+    c=a+b;
+    d=a*b;
+    
+    //Output the results
+    cout<<"a = "<<a<<endl;
+    cout<<"b = "<<b<<endl;
+    cout<<c<< "=" <<a<< "+" <<b<<endl;
+    cout<<"This is the end of the program."<<endl;
+    //Exit
+    
+    return 0;
+
+                                }
