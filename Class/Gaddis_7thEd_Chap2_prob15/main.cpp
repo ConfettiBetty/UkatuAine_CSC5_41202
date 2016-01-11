2b8b19d4464c94e21ac0fd99f85fc24daf2656b0
ConfettiBetty <noxiousbyproxy@yahoo.com>
2016-01-10 23:42:44 -0800

    Signed-off-by: ConfettiBetty <noxiousbyproxy@yahoo.com>

---
+++ Class/Gaddis_7thEd_Chap2_prob15/main.cpp
@@ -0,0 +1,28 @@
+/* 
+ * Author: Aine Ukatu
+ * Created on January 10, 2016, 11:31 PM
+ * Purpose:  Gaddis 7thEd Chapter 2 Problem 15
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Declare variable
+char star='*';
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+        
+    //Output the results
+    cout<<"   "<<star<<"   "<<endl;
+    cout<<"  "<<star<<star<<star<<"  "<<endl;
+    cout<<" "<<star<<star<<star<<star<<star<<" "<<endl;
+    cout<<star<<star<<star<<star<<star<<star<<star<<endl;
+    
+    //Exit stage right
+    return 0;
+}
+
