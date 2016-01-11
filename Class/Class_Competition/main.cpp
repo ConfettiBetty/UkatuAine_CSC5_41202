8808c9a7619d6430fea172ad374cd084b600e1ed
ConfettiBetty <noxiousbyproxy@yahoo.com>
2016-01-11 12:10:08 -0800

    Signed-off-by: ConfettiBetty <noxiousbyproxy@yahoo.com>

---
+++ Class/Class_Competition/main.cpp
@@ -0,0 +1,55 @@
+/* 
+    File:   main.cpp
+    Author: Aine Ukatu & David Desforges
+    Created on January 11, 2016, 11:25 AM
+    Purpose: Class Competition
+  */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare and initialize variables
+    bool pass=true;
+    bool fail=false;
+    
+    pass>=70;
+    fail<70;   
+    
+    int score1=80;
+    int score2=60;
+    
+    //Create Heading
+    cout<<"Did you pass or did you fail?"<<endl;
+       
+    cout<<"Your score 1 is: "<<(score1?'Pass':'Fail');endl;
+    cout<<"Your score 2 is: "<<(score2?'Pass':'Fail');endl;
+    
+
+//Question 2: Overtime calculation
+    //Declare and initialize variables
+    
+    
+    
+    float pyrtReg=10;
+    float hrsWkd1=30;
+    float hrsWkd2=50;
+    float ovrTme;        
+    
+    //Determine if regular or overtime pay, and amount
+    ovrTme=1.5*pyrtReg;
+    cout<<""
+
+
+    
+    //Exit stage right
+    return 0;
+}
