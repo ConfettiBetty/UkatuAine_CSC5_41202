464061bc18dfabebf50416e00c68d21b1f5015b1
ConfettiBetty <noxiousbyproxy@yahoo.com>
2016-01-10 20:10:11 -0800

    Signed-off-by: ConfettiBetty <noxiousbyproxy@yahoo.com>

--- Class/Gaddis_9thEd_Chap2_ProbProj_1/main.cpp
+++ Class/Gaddis_9thEd_Chap2_ProbProj_1/main.cpp
@@ -13,28 +13,26 @@ using namespace std;
 //Global Constant
 const char lethMou=5;//Amount artificial sweetener lethal to lab mouse, grams
 const char massMou=35;//Mass of lab mouse, grams
-const char sodMass=350;//Total mass of 1 unit soda, grams
-const float q=0.001;   //Amount artificial sweetener per unit soda, grams
 
 //Execution Begins Here
 int main(int argc, char** argv) {
     //Declare and initialize variables
     int cans;        //Number of whole soda can units
-    int  wtGoal;     //Desired goal weight, grams
-    float  lethHum;  //Amount artificial sweetener lethal to human, grams
-    
+    float sodMass=350; //Total mass of 1 unit soda, grams
+    float  wtGoal;     //Desired goal weight, grams
+    float  lethHum;    //Amount artificial sweetener lethal to human, grams
+    float q=0.001;     //Amount artificial sweetener per unit soda, grams
     
     //Calculate how many cans of soda are lethal
-    lethHum=(5*wtGoal)/7;
+    cout<<"Welcome to the diet soda weight loss program!"<<endl;
+    cout<<"What is your desired weight (grams)?"<<endl;
+    cin>>wtGoal;
+    lethHum=wtGoal/7;
     cans=(lethHum/q)*1/sodMass;
-    cin>>"Welcome to the diet soda weight loss program.">>endl;
-    cin>>"What is your desired weight (grams)?">>endl;
-    cin>>wtGoal>>endl;
-    
     
     //Output the results
-    cout<<"Soda can limit = about "<<static_cast<int>(cans)<<" cans"<<endl;
-    cout<<"Artificial sweetener allowance: "<<static_cast<float>(lethHum)<<"grams"<<endl;
+    cout<<"Soda can limit : about "<<cans<<" cans"<<endl;
+    cout<<"Artificial sweetener allowance: "<<lethHum<<" grams"<<endl;
 
     
     
