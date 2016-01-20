557548fbf01934d7eb70e3b59b7c61d30cffe24a
ConfettiBetty <noxiousbyproxy@yahoo.com>
2016-01-20 11:26:02 -0800

    Signed-off-by: ConfettiBetty

---
+++ Homework/Assignment 3/Gaddis8thEd_Ch5Prob7/main.cpp
@@ -0,0 +1,42 @@
+/* 
+    File:   main.cpp
+    Author: Aine (Phyllis) Ukatu
+    Created on January 20, 2016, 11:12 AM
+    Purpose: Double your pennies everyday! A salary table.
+ */
+
+//System Libraries
+#include <iostream> //I/O
+#include <iomanip>  //Formatting
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+
+//Function prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //The problem to solve
+    cout<<endl<<"Gaddis 8thEd Chapter 5 Problem 7"<<endl;
+    cout<<endl<<"Salary Table Display"<<endl<<endl;
+    
+    //Declare variables
+    unsigned int salary=1;      //Salary starting as a penny
+    float totPay=salary;          //Sum of the salaries
+    
+    //Loop to generate the salary table with total paid
+    cout<<" Day           Salary($)      Total($)"<<endl;
+    cout<<fixed<<setprecision(2)<<showpoint;
+    cout<<setw(4)<<1<<setw(15)<<salary/100.0f<<setw(15)<<totPay/100.0f<<endl;
+    for(int day=2;day<=31;day++){
+        //salary*=2;
+        salary<<=1;
+        totPay+=salary;
+        cout<<setw(4)<<day<<setw(15)<<salary/100.0f<<setw(15)<<totPay/100.0f<<endl;
+    }
+    //Exit stage right
+    return 0;
+}
--- Homework/Assignment 3/Savitch7thEd_Ch3Prob1_RockPaperScissrV2/main.cpp
+++ Homework/Assignment 3/Savitch7thEd_Ch3Prob1_RockPaperScissrV2/main.cpp
@@ -26,12 +26,16 @@ int main(int argc, char** argv) {
     cout<<endl<<"Savitch 7thEd Chapter 3 Problem 1"<<endl;
     cout<<endl<<"The Rock Paper Scissors Game"<<endl;
     
-    //Set the random number seed
+    //Set the random number seed and declare the question
     srand(static_cast<unsigned int>(time(0)));
+    
+    //Loop until the player wants to quit
+    do{
     
     //Declare and initialize variables
     char computr; //Computer's play hand value
     char player;  //Player's move
+    char qwstion; //Question, does player want to keep playing
     
     //Input player's turn
     do{
@@ -40,16 +44,31 @@ int main(int argc, char** argv) {
         player=toupper(player);
     }while(!(player=='P'||player=='R'||player=='S'));
     
-    
     //Computer generated play
     do{
         computr=rand()%4+80;
     }while(computr=='Q');
         
-    //Output the results
+    //Output the moves by computer and player
     cout<<"The computer played "<<computr<<endl;
     cout<<"The Players move "<<player<<endl;
     
+    //Determine the result
+    if(computr==player)cout{
+        cout<<"The result is a tie"<<endl;
+    }else if(player=='P'&&computr=='R'){
+            cout<<"The Player wins!"<<endl;
+    }else if(player=='R'&&computr=='S'){
+            cout<<"The Player wins!"<<endl;
+    }else if(player=='S'&&computr=='P'){
+            cout<<"The Player wins!"<<endl;
+    }else{
+        cout<<"The Player loses. :-("<<endl;
+
+    //Kepp playing?
+    cout<<end<<"Do you want to continue playing?"<<endl;
+    cin>>qwstion;
+    }while(toupper(qwstion)=='Y');
     //Exit stage right
     return 0;
 }
