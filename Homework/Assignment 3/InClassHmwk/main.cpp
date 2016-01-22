fde2c017b2dc95e20b9558ee3d21dc9599880e7e
Ray Anderson <12001002405397@fedidcard.gov>
2016-01-21 18:31:01 -0800

    Signed-off-by: ConfettiBetty

--- Homework/Assignment 3/InClassHmwk/main.cpp
+++ Homework/Assignment 3/InClassHmwk/main.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <cmath>
 #include <cstdlib>
+#include <iomanip>
 
 using namespace std;
 
@@ -21,21 +22,18 @@ float PI=3.1415;
 
 //Execution Begins Here
 int main(int argc, char** argv) {
-    //Declare and initialize variables for e to the x and N factorial
-    float nTerm;                      //Determine how many terms
-    float tol=1e-3f,x;//variables to help find pi
-    float pie;                   //arctan(1)*4 = pi
+    //Declare and initialize variables for finding pi
+    float piCalc;         //Calculated value of pi arctan(1)*4 = pi
+    float piDiff;         //Difference between calculated pi and global constant
     
     //Calculate pi
-    for(nTerm=1,pie=4;x>tol;nTerm++)
-    {
-        pie=pie+4*(pow(-1,nTerm)/(2*nTerm+1));
-        x=abs(pie-PI);
-    }
+    piCalc=atan(1)*4;
+    piDiff=piCalc-PI;
+    
     //Output the results
     cout<<"The known constant value of pi is "<<PI<<"."<<endl;
-    cout<<"The number of terms for the approximated pi value, "<<pie<<", is "<<nTerm<<endl;
-    cout<<"The tolerance is "<<tol<<", and the difference is "<<x<<"."<<endl;
+    cout<<"The calculated value for pi is "<<setprecision(4)<<piCalc<<endl;
+    cout<<"The difference is "<<setprecision(4)<<piDiff<<"."<<endl;
     
     //Exit stage right
     return 0;
--- Homework/Assignment 3/InClassHmwk/nbproject/Makefile-variables.mk
+++ Homework/Assignment 3/InClassHmwk/nbproject/Makefile-variables.mk
@@ -7,21 +7,21 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=Cygwin_4.x-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x-Windows
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
 CND_ARTIFACT_NAME_Debug=inclasshmwk
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/inclasshmwk
-CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x-Windows/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/inclasshmwk
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
 CND_PACKAGE_NAME_Debug=inclasshmwk.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/inclasshmwk.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/inclasshmwk.tar
 # Release configuration
-CND_PLATFORM_Release=Cygwin_4.x-Windows
-CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x-Windows
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
 CND_ARTIFACT_NAME_Release=inclasshmwk
-CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/inclasshmwk
-CND_PACKAGE_DIR_Release=dist/Release/Cygwin_4.x-Windows/package
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/inclasshmwk
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
 CND_PACKAGE_NAME_Release=inclasshmwk.tar
-CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/inclasshmwk.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/inclasshmwk.tar
 #
 # include compiler specific variables
 #
